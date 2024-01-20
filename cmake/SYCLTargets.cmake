8fe2d64e7178fa1e38045a018c07757c99be8429
Lukas Riebel <quxflux@theriebel.com>
2024-01-20 10:58:48 +0100

    remove mention of CUDA from SYCL CMake targets

--- cmake/SYCLTargets.cmake
+++ cmake/SYCLTargets.cmake
@@ -1,5 +1,5 @@
 add_library(sycl_target INTERFACE)
-#set(CUDA_MEDIAN_FILTER_SYCL_FLAGS -fsycl -fsycl-targets=nvptx64-nvidia-cuda)
-set(CUDA_MEDIAN_FILTER_SYCL_FLAGS -fsycl)
+
+set(CUDA_MEDIAN_FILTER_SYCL_FLAGS -fsycl -fsycl-targets=nvptx64-nvidia-cuda)
 target_compile_options(sycl_target INTERFACE ${CUDA_MEDIAN_FILTER_SYCL_FLAGS})
 target_link_options(sycl_target INTERFACE ${CUDA_MEDIAN_FILTER_SYCL_FLAGS})
